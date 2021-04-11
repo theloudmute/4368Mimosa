da89c3afa8180a7c555ee6bc596d00eecd74d395
theloudmute <42556429+theloudmute@users.noreply.github.com>
2021-04-11 14:03:16 -0500

    Create agent.py

---
+++ agent.py
@@ -0,0 +1,20 @@
+class agent:
+  def __init__(self):
+    # (i,j) is the position of the agent
+    self.i = 5
+    self.j = 1
+    # 1 if agent is carrying a package and 0 otherwise
+    self.x = 0
+    # number of blocks in cell (1,1)
+    self.a = 0
+    # number of blocks in cell (1,5)
+    self.b = 0
+    # number of blocks in cell (3,3)
+    self.c = 0
+    # number of blocks in cell (3,5)
+    self.d = 8
+    # number of blocks in cell (4,2)
+    self.e = 8
+    # number of blocks in cell (5,5)
+    self.f = 0
+    
